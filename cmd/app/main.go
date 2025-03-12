47fc351cf1f2e75f666a275fb07c239050fd1bfd
Guizzs26 <guiteixeira1926@gmail.com>
2025-03-12 17:29:58 -0300

    feat: add createWindow function which is responsible for creating the main window, the input elements, the button and initializing the window

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,77 +1,21 @@
 package main
 
 import (
-	"time"
+	"fmt"
 
-	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
 
-func updateTime(clock *widget.Label) {
-	formatted := time.Now().Format("Time 03:04:05")
-	clock.SetText(formatted)
-}
-
 func main() {
-	app := app.New()                 // Cria uma nova aplicação do Fyne. Como se fosse uma nova instância do Fyne.
-	window := app.NewWindow("Clock") // Cria uma nova janela com o TÍTULO "Hello World".
-	window.SetMaster()               // Podemos definir uma janela principal para nossa aplicação. Podemos ter uma janela principal e várias secundárias.
-	// Se a "master" for fechada, toda aplicação será encerrada automaticamente.
-	window.Resize(fyne.NewSize(400, 400)) // Modifica o tamanho da janela.
+	app := app.New()
+	window := app.NewWindow("Window")
 
-	// Aqui estamos aprendendo a atualizar um valor. Precisamos passar uma label (por exemplo) vazia e atribuir para uma variável, pois o retorno dessa função é uma referência
-	// à label criada, então podemos atualiza-la depois.
-	// formatted := time.Now().Format("Time 03:04:05")
-	// clock.SetText(formatted)
-	clock := widget.NewLabel("")
-	window.SetContent(clock)
-	updateTime(clock)
+	firstName := widget.NewEntry()
+	lastName := widget.NewEntry()
 
-	// window.SetContent(widget.NewLabel("Hello World")) // Define o conteúdo dessa janela através de uma label fixa.
+	fmt.Println(firstName)
+	fmt.Println(lastName)
 
-	// IIFE -> Immediately Invoked Function Expression, é uma função anônima que é automaticamente criada e executada. Usamos uma goroutine também para executar o código de forma assíncrona.
-	// Aqui que mora a magia da execução em segundo plano de forma concorrente (pararelo) !!!
-	go func() {
-		// Isso é basicamente um for infinito que fica atualizando o valor de clock a cada segundo.
-		for range time.Tick(time.Second) {
-			updateTime(clock)
-		}
-	}()
-	window.Show()
-
-	// Criação de uma segunda janela. Como explicado anteriormente, precisamos agora chamar o Show() individualmente para cada uma.
-	window2 := app.NewWindow("Second window")
-	window2.SetContent(widget.NewLabel("Second - More content!"))
-	window2.Show()
-
-	// Podemos criar janelas de forma dinâmica:
-	window2.SetContent(widget.NewButton("Open new", func() {
-		window3 := app.NewWindow("Third window")
-		window3.SetContent(widget.NewLabel("Third - More content!!!"))
-		window3.Show()
-	}))
-
-	app.Run()
-	// window.ShowAndRun() // Exibe a janela e roda a aplicação
-	/*
-		Também podemos fazer da seguinte forma:
-
-		window.Show()
-		app.Run()
-
-
-		- É importante ressaltar que apenas podemos ter um único "Run()" na aplicação. Essas aplicações de GUI precisam executar uma
-			espécie de "event loop" que fica "escutando" por interações do usuário com a aplicação.
-		- Essas funções Run() e ShowAndRun() são as funções responsáveis por iniciar esse loop de eventos.
-		- Ela deve ser chamada no final do código de configuração da aplicação Fyne, normalmente na main().
-
-		- A principal diferença entre o ShowAndRun() e o Run() é que a primeira função é usada quando você tem APENAS UMA JANELA PRINCIPAL (inicia o loop e a janela)
-			e a segunda é usada quando você tem várias janelas (apenas inicia o loop, cada janela deve ser chamada manualmente com a função Show()).
-
-		- Qualquer código após a função Run() ou ShowAndRun() só será executado APÓS o fechamento da aplicação. Podemos usar o app.Quit() para fechar
-			aplicações Desktop (não funciona para Android ou IOS).
-		- Se todas as janelas forem fechadas, o programa se encerra automaticamente.
-
-	*/
+	window.ShowAndRun()
 }
--- internal/ui/window.go
+++ internal/ui/window.go
@@ -1,3 +1,59 @@
 package ui
 
-func createWindow() {}
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+/*
+Responsável por:
+
+- Criar a janela principal
+- Criar os campos de input (com o widget do Fyne)
+- Adicionar o botão "Gerar excel"
+- Organizar esses componentes com layouts do container do Fyne
+- Exibir a janela
+*/
+func createWindow() {
+	app := app.New()
+	mainWindow := app.NewWindow("Excel generator")
+
+	mainWindow.Resize(fyne.NewSize(400, 300))
+
+	firstNameEntry := widget.NewEntry()
+	firstNameEntry.SetPlaceHolder("Enter your first name")
+
+	lastNameEntry := widget.NewEntry()
+	lastNameEntry.SetPlaceHolder("Enter your last name")
+
+	ageEntry := widget.NewEntry()
+	ageEntry.SetPlaceHolder("Enter your age")
+
+	birthdayEntry := widget.NewEntry()
+	birthdayEntry.SetPlaceHolder("Enter your birthday date (dd/mm/aaaa)")
+
+	randomFloatValueEntry := widget.NewEntry()
+	randomFloatValueEntry.SetPlaceHolder("Enter any decimal value")
+
+	generateExcelButton := widget.NewButton("Generate Excel Spreadsheet", func() {
+		fmt.Println("Press button")
+	})
+
+	content := container.NewVBox(
+		widget.NewLabel("Fill in the data to generate the Excel"),
+		firstNameEntry,
+		lastNameEntry,
+		ageEntry,
+		birthdayEntry,
+		randomFloatValueEntry,
+		generateExcelButton,
+	)
+
+	mainWindow.SetContent(content)
+
+	mainWindow.ShowAndRun()
+}
